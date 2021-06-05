0cb77944c24caa75b2c58d4cd2b7f6a14bca4acb
Navin Kumar <64195592+Navinkt@users.noreply.github.com>
2021-06-05 21:57:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,159 @@
+var playSound = {
+          
+    a : {
+        color : '#00FFFF',
+        sound : new Howl({
+            src: ['sounds/bubbles.mp3']
+          })
+    },
+    b : {
+        color : '#5DFC0A',
+        sound : new Howl({
+            src: ['sounds/clay.mp3']
+          }) 
+    },
+    c : {
+        color : '#8A2BE2'	,
+        sound : new Howl({
+            src: ['sounds/confetti.mp3']
+          }) 
+    },
+    d : {
+        color : '#993300'	,
+        sound : new Howl({
+            src: ['sounds/corona.mp3']
+          })
+    },
+    e : {
+        color : '#A020F0'	,
+        sound : new Howl({
+            src: ['sounds/dotted-spiral.mp3']
+          })
+    },
+    f : {
+        color : '#FFFF00',
+        sound : new Howl({
+            src: ['sounds/flash-1.mp3']
+          })
+    },
+    g : {
+        color : '#FF82AB'	,
+        sound : new Howl({
+            src: ['sounds/flash-2.mp3']
+          }) 
+    },
+    h : {
+        color : '#FF8247'	,
+        sound : new Howl({
+            src: ['sounds/flash-3.mp3']
+          }) 
+    },
+    i : {
+        color : '#FF3D0D'	,
+        sound : new Howl({
+            src: ['sounds/glimmer.mp3']
+          }) 
+    },
+    j : {
+        color : '#CECC15'	,
+        sound : new Howl({
+            src: ['sounds/moon.mp3']
+          })
+    },
+    k : {
+        color : '#003EFF'	,
+        sound : new Howl({
+            src: ['sounds/pinwheel.mp3']
+          })
+    },
+    l : {
+        color : '#FF7D40'	,
+        sound : new Howl({
+            src: ['sounds/piston-1.mp3']
+          })
+    },
+    m : {
+        color : '#DC8909'	,
+        sound : new Howl({
+            src: ['sounds/piston-2.mp3']
+          })
+    },
+    n : {
+        color : '#CCFFCC'	,
+        sound : new Howl({
+            src: ['sounds/piston-3.mp3']
+          })
+    },
+    o : {
+        color : 	'#ff6600'	,
+        sound : new Howl({
+            src: ['sounds/splits.mp3']
+          })
+    },
+    p : {
+        color : '#660033'	,
+        sound : new Howl({
+            src: ['sounds/squiggle.mp3']
+          }) 
+    },
+    q : {
+        color : '#00ffff'	,
+        sound : new Howl({
+            src: ['sounds/strike.mp3']
+          })
+    },
+    r : {
+        color : 	'#1aff66'	,
+        sound : new Howl({
+            src: ['sounds/suspension.mp3']
+          })
+    },
+    s : {
+        color : '#999966'	,
+        sound : new Howl({
+            src: ['sounds/timer.mp3']
+          })
+    },
+    t : {
+        color : '#ffff00'	,
+        sound : new Howl({
+            src: ['sounds/ufo.mp3']
+          })
+    },
+    u : {
+        color : '#990000'	,
+        sound : new Howl({
+            src: ['sounds/veil.mp3']
+          }) 
+    },
+    v : {
+        color : '#990099'	,
+        sound : new Howl({
+            src: ['sounds/wipe.mp3']
+          })
+    },
+    w : {
+        color : '#663300'	,
+        sound : new Howl({
+            src: ['sounds/zig-zag.mp3']
+          })
+    },
+    x : {
+        color : '#660066'	,
+        sound : new Howl({
+            src: ['sounds/prism-1.mp3']
+          })
+    },
+    y : {
+        color : 	'#6600cc',
+        sound : new Howl({
+            src: ['sounds/prism-2.mp3']
+          })
+    },
+    z : {
+        color : '#993366'	,
+        sound : new Howl({
+            src: ['sounds/prism-3.mp3']
+          }) 
+    }
+}
